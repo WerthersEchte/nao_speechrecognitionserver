9e75b9929036f24cb6d237c1093eec709dcaaafa
WerthersEchte <arabali@gmx.de>
2014-06-24 11:41:21 +0200

    bugfix

--- core/servercore.js
+++ core/servercore.js
@@ -15,6 +15,7 @@ var logger = logging.getLogger( 'core' );
 
 var express = require('express');
 var https = require('https');
+var http = require('http');
 fs = require("fs");
 
 var app = express();
@@ -23,11 +24,13 @@ var privateKey  = fs.readFileSync(process.cwd() + '/core/server.key', 'utf8');
 var certificate = fs.readFileSync(process.cwd() + '/core/server.crt', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
-var server = https.createServer(credentials, app);
+var httpsserver = https.createServer(credentials, app);
+var server = http.createServer(app);
 
 app.use( express.logger( 'dev' ) );
 app.use(express.static( process.cwd() + '/public'));
-server.listen( 11972 );
+httpsserver.listen( 11972 );
+server.listen( 11973 );
 
 webSockets.attach( server );
 tcpConnection.createServer( 9090 );
